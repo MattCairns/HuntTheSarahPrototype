c75739c24b4643e63f162a0a3838b2c73b61cc74
Matthew Cairns <mattrcairns@gmail.com>
2014-05-06 19:26:29 +0100

    First Commit.

---
+++ android/src/com/matthewcairns/flameblade/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.matthewcairns.flameblade.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.matthewcairns.flameblade.Flameblade;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Flameblade(), config);
+	}
+}
---
+++ html/src/com/matthewcairns/flameblade/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.matthewcairns.flameblade.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.matthewcairns.flameblade.Flameblade;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Flameblade();
+        }
+}
