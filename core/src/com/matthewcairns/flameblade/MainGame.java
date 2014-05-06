dc503843c571a868058f0733f29a6460f024b450
Matthew Cairns <mattrcairns@gmail.com>
2014-05-06 19:54:33 +0100

    Implemented skeleton of MainGame

---
+++ core/src/com/matthewcairns/flameblade/MainGame.java
@@ -0,0 +1,45 @@
+package com.matthewcairns.flameblade;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by Matthew Cairns on 06/05/2014.
+ * All rights reserved.
+ */
+public class MainGame implements Screen {
+    final Flameblade game;
+
+    Stage stage;
+
+    OrthographicCamera camera;
+
+    public MainGame(final Flameblade gam) {
+        game = gam;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resume() {}
+    @Override
+    public void pause() {}
+    @Override
+    public void resize(int width, int height) {}
+    @Override
+    public void hide() {}
+    @Override
+    public void show() {}
+    @Override
+    public void dispose() {}
+}
